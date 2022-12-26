8fd9cfe200b53b9d47ea0f3a2fced8631d1bb46a
Cansin Comert <cansincomert@gmail.com>
2022-12-26 09:35:20 +0300

    unit testing
    UserCreateForm unit tests added

--- collectorApp/collector/tests.py
+++ collectorApp/collector/tests.py
@@ -1,20 +1,37 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-from groups.models import User
-# Create your tests here.
+from .forms import UserCreateForm
 
-class LoginTestCase(TestCase):
+class UserCreateFormTestCase(TestCase):
     def setUp(self):
-        # Create a user
+        self.form = UserCreateForm()
+
+    def test_form_has_fields(self):
+        # Test that the form has the correct fields
+        expected_fields = ['username', 'email', 'password1', 'password2']
+        self.assertSequenceEqual(expected_fields, list(self.form.fields))
+    
+    def test_form_validation(self):
+        # Test that the form requires all fields to be filled in
+        form_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password1': 'testpass',
+            'password2': 'testpass'
+        }
+        form = UserCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+        # Test that the form requires the passwords to match
+        form_data['password2'] = 'wrongpass'
+        form = UserCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+        # Test that the form requires a unique username
+        User = get_user_model()
         User.objects.create_user(username='testuser', password='testpass')
-
-    def test_login(self):
-        # Send a POST request to the login URL with the user's credentials
-        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'testpass'})
-
-        # Check that the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
-
-        # Check that the user was authenticated and is now logged in
-        self.assertTrue(response.context['user'].is_authenticated)
+        form_data['username'] = 'testuser'
+        form_data['password2'] = 'testpass'
+        form = UserCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
